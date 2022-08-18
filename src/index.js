af74f70a639146dbbcb248e9788ea79708b75087
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-08-18 14:12:02 +0300

    install lodash

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  inputField: document.querySelector(`#search-box`),
+  countryList: document.querySelector(`.country-list`),
+  countryInfo: document.querySelector(`.country-info`),
+  background: document.querySelector(`.background`),
+};
+
+refs.inputField.addEventListener(
+  `input`,
+  debounce(onInputSearch, DEBOUNCE_DELAY)
+);
+
+function fetchCountries(name) {
+  const r = fetch(`https://restcountries.com/v3.1/name/${name}`).then(data => {
+    console.log(data);
+  });
+}
+
+// fetchCountries('peru');
+
+function onInputSearch(e) {
+  const result = fetchCountries(e.target.value);
+  console.log(result);
+}
