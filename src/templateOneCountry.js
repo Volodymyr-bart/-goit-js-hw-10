07038c788fecd6278657346cdc15f1fa7766056c
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-08-21 22:10:50 +0300

    Update templateOneCountry.js

--- src/templateOneCountry.js
+++ src/templateOneCountry.js
@@ -1,3 +1,8 @@
 export default function templateOneCountry(params) {
   console.log(params);
+  const { capital, population, flags, languages } = params[0];
+  console.dir(capital);
+  console.log(population);
+  console.log(flags);
+  console.log(languages);
 }
